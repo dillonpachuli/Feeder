1e0c05d9477ad8831a0fe1f141f7239019750748
Dillon Chapuli <148895731+dillonpachuli@users.noreply.github.com>
2024-09-27 16:33:26 +0000

    final commit done

--- Feeder.java
+++ Feeder.java
@@ -37,7 +37,18 @@ public class Feeder {
      */
     public int simulateManyDays(int numBirds, int numDays) 
     {
-        return 0;
+        int days = 0;
+        while (numDays > 0)
+        {
+            if (currentFood == 0)
+            {
+                return days;
+            }
+            simulateOneDay(numBirds);
+            numDays--;
+            days++;
+        }
+        return days;
     }
 
     public int getCurrentFood()
--- Main.java
+++ Main.java
@@ -2,16 +2,30 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //Feeder f = new Feeder();
-        //System.out.println(f);
-        //System.out.println(f.getCurrentFood());
+        Feeder f = new Feeder();
+        System.out.println(f);
+        System.out.println(f.getCurrentFood());
+        
         Feeder g = new Feeder(500);
-        //System.out.println(g);
-        //System.out.println(g.getCurrentFood());
-        //System.out.println(randomInt);
+        System.out.println(g);
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
+
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h.getCurrentFood());
 
         Feeder i = new Feeder(100);
         i.simulateOneDay(5);
         System.out.println(i);
+
+        Feeder j = new Feeder(2400);
+        System.out.println(j.simulateManyDays(10, 4));
+
+        Feeder o = new Feeder(250);
+        System.out.println(o.simulateManyDays(10, 5));
+
+        Feeder k = new Feeder(0);
+        System.out.println(k.simulateManyDays(5, 10));
     }
 }
